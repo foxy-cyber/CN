3bb5b3a4c73ac89d760a896619dbdfee02d97380
Foxy <86040276+foxy-cyber@users.noreply.github.com>
2023-12-30 21:23:24 +0530

    Update p2.tcl

--- p2.tcl
+++ p2.tcl
@@ -4,8 +4,6 @@ $ns trace-all $tf
 set nf [open prog2.nam w]
 $ns namtrace-all $nf
 set cwind [open win2.tr w]
-$ns color 1 Blue
-
 set n1 [$ns node]
 set n2 [$ns node]
 set n3 [$ns node]
@@ -13,19 +11,18 @@ set n4 [$ns node]
 set n5 [$ns node]
 set n6 [$ns node]
 $ns duplex-link $n1 $n3 2Mb 2ms DropTail
-$ns duplex-link $n2 $n3 2Mb 2ms DropTail
+$ns duplex-link $n2 $n3 1Mb 2ms DropTail
 $ns duplex-link $n3 $n4 2Mb 2ms DropTail
 $ns duplex-link $n4 $n5 2Mb 2ms DropTail
 $ns duplex-link $n4 $n6 2Mb 2ms DropTail
-
 set tcp0 [new Agent/TCP]
-$ns attach-agent $n1 $tcp0 
+$ns attach-agent $n1 $tcp0
 set sink0 [new Agent/TCPSink]
 $ns attach-agent $n6 $sink0
 $ns connect $tcp0 $sink0
-set ftp [new Application/FTP]
-$ftp attach-agent $tcp0
-
+set ftp0 [new Application/FTP]
+$ftp0 attach-agent $tcp0
+$ns at 1.1 "$ftp0 start"
 set tcp1 [new Agent/TCP]
 $ns attach-agent $n2 $tcp1
 set sink1 [new Agent/TCPSink]
@@ -35,24 +32,29 @@ set tel1 [new Application/Telnet]
 $tel1 attach-agent $tcp1
 $ns at 0.1 "$tel1 start"
 $ns at 10 "finish"
-
+$ns color 1 Blue
+$ns color 2 Red
+$tcp0 set fid_ 1
+$tcp1 set fid_ 2
 proc plotWindow {tcpSource file} {
-    global ns 
-    set time 0.01 
-    set now [$ns now]
-    set cwd0 [$tcpSource set cwnd_]
-    puts $file "$now $cwd0"
-    $ns at [expr $now+$time] "plotWindow $tcpSource $file"
+global ns
+set time 0.01
+set now [$ns now]
+set cwnd0 [$tcpSource set cwnd_]
+puts $file "$now $cwnd0"
+$ns at [expr $now+$time] "plotWindow $tcpSource $file"
 }
-proc finish { } {
-        global ns tf nf cwind
-        $ns flush-trace
-        close $tf 
-        close $nf 
-        puts "Running nam..."
-        exec nam prog2.nam &
-        exec xgraph win2.tr &
-        exit 0
+$ns at 2.0 "plotWindow $tcp0 $cwind"
+$ns at 5.5 "plotWindow $tcp1 $cwind"
+proc finish {} {
+global ns tf nf cwind
+$ns flush-trace
+close $nf
+close $tf
+puts "Running nam..."
+exec nam prog2.nam &
+exec xgraph win2.tr &
+exit 0
 }
 $ns run
 
