f7b64f80a3444c900caeacc0bcd7869ae5743f62
Tilak <tilaksk2003@gmail.com>
2024-01-16 13:47:56 +0530

    -

--- p10.tcl
+++ p10.tcl
@@ -2,25 +2,24 @@ set val(chan) Channel/WirelessChannel
 set val(prop) Propagation/TwoRayGround
 set val(netif) Phy/WirelessPhy
 set val(mac) Mac/802_11
-#set val(ifq) Queue/DropTail/PriQueue
-set val(ifq) CMUPriQueue
+set val(ifq) Queue/DropTail/PriQueue
 set val(ll) LL
 set val(ant) Antenna/OmniAntenna
-set val(x) 700
-set val(y) 700
+set val(x) 500
+set val(y) 500
 set val(ifqlen) 50
-set val(nn) 6
-set val(stop) 60.0
-set val(rp) DSR
+set val(nn) 5
+set val(stop) 50.0
+set val(rp) AODV
 set ns_ [new Simulator]
-set tracefd [open 004.tr w]
+set tracefd [open 006.tr w]
 $ns_ trace-all $tracefd
-set namtrace [open 004.nam w]
+set namtrace [open 006.nam w]
 $ns_ namtrace-all-wireless $namtrace $val(x) $val(y)
 set prop [new $val(prop)]
 set topo [new Topography]
 $topo load_flatgrid $val(x) $val(y)
-set god_ [create-god $val(nn)]
+create-god $val(nn)
 #Node Configuration
 $ns_ node-config -adhocRouting $val(rp) \
 -llType $val(ll) \
@@ -34,52 +33,49 @@ $ns_ node-config -adhocRouting $val(rp) \
 -topoInstance $topo \
 -agentTrace ON \
 -routerTrace ON \
--macTrace ON
+-macTrace ON \
+-IncomingErrProc "uniformErr" \
+-OutgoingErrProc "uniformErr"
+proc uniformErr {} {
+set err [new ErrorModel]
+$err unit pkt
+$err set rate_ 0.01
+return $err
+}
 #Creating Nodes
 for {set i 0} {$i < $val(nn) } {incr i} {
 set node_($i) [$ns_ node]
 $node_($i) random-motion 0
 }
 #Initial Positions of Nodes
-$node_(0) set X_ 150.0
-$node_(0) set Y_ 300.0
-$node_(0) set Z_ 0.0
-$node_(1) set X_ 300.0
-$node_(1) set Y_ 500.0
-$node_(1) set Z_ 0.0
-$node_(2) set X_ 500.0
-$node_(2) set Y_ 500.0
-$node_(2) set Z_ 0.0
-$node_(3) set X_ 300.0
-$node_(3) set Y_ 100.0
-$node_(3) set Z_ 0.0
-$node_(4) set X_ 500.0
-$node_(4) set Y_ 100.0
-$node_(4) set Z_ 0.0
-$node_(5) set X_ 650.0
-$node_(5) set Y_ 300.0
-$node_(5) set Z_ 0.0
 for {set i 0} {$i < $val(nn)} {incr i} {
 $ns_ initial_node_pos $node_($i) 40
 }
 #Topology Design
-$ns_ at 1.0 "$node_(0) setdest 160.0 300.0 2.0"
-$ns_ at 1.0 "$node_(1) setdest 310.0 150.0 2.0"
-$ns_ at 1.0 "$node_(2) setdest 490.0 490.0 2.0"
-$ns_ at 1.0 "$node_(3) setdest 300.0 120.0 2.0"
-$ns_ at 1.0 "$node_(4) setdest 510.0 90.0 2.0"
-$ns_ at 1.0 "$node_(5) setdest 640.0 290.0 2.0"
-$ns_ at 4.0 "$node_(3) setdest 300.0 500.0 5.0"
+$ns_ at 1.0 "$node_(0) setdest 10.0 10.0 50.0"
+$ns_ at 1.0 "$node_(1) setdest 10.0 100.0 50.0"
+$ns_ at 1.0 "$node_(4) setdest 50.0 50.0 50.0"
+$ns_ at 1.0 "$node_(2) setdest 100.0 100.0 50.0"
+$ns_ at 1.0 "$node_(3) setdest 100.0 10.0 50.0"
 #Generating Traffic
 set tcp0 [new Agent/TCP]
 set sink0 [new Agent/TCPSink]
 $ns_ attach-agent $node_(0) $tcp0
-$ns_ attach-agent $node_(5) $sink0
+$ns_ attach-agent $node_(2) $sink0
 $ns_ connect $tcp0 $sink0
 set ftp0 [new Application/FTP]
 $ftp0 attach-agent $tcp0
-$ns_ at 5.0 "$ftp0 start"
-$ns_ at 60.0 "$ftp0 stop"
+$ns_ at 1.0 "$ftp0 start"
+$ns_ at 50.0 "$ftp0 stop"
+set tcp1 [new Agent/TCP]
+set sink1 [new Agent/TCPSink]
+$ns_ attach-agent $node_(1) $tcp1
+$ns_ attach-agent $node_(2) $sink1
+$ns_ connect $tcp1 $sink1
+set ftp1 [new Application/FTP]
+$ftp1 attach-agent $tcp1
+$ns_ at 1.0 "$ftp1 start"
+$ns_ at 50.0 "$ftp1 stop"
 #Simulation Termination
 for {set i 0} {$i < $val(nn) } {incr i} {
 $ns_ at $val(stop) "$node_($i) reset";
